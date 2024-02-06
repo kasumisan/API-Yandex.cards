4346197c7a7dcd2104c85f48dd7142d97709797a
itcube2023 <saekirisan@gmail.com>
2024-02-06 16:30:15 +0500

    1 задание

--- main.py
+++ main.py
@@ -27,3 +27,30 @@ def load_map(mp):
 
     map_file = "map.png"
 
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except IOError as ex:
+        print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
+
+    return map_file
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    mp = MapParams()
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            break
+        map_file = load_map(mp)
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    os.remove(map_file)
+
+
+if __name__ == "__main__":
+    main()
